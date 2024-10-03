0d912b771f22b01aedc39f93929f06b5fa37d9f0
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 14:59:39 +0300

    Add project files.

---
+++ BasicOperations/writingOnConsole.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BasicOperations
+{
+    internal class writingOnConsole
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello world"); 
+            //Console.Write("Hello world");
+            /*
+            Console.Write("Name= ");
+            string name = Console.ReadLine();
+            Console.Write("Surname= ");
+            string surname = Console.ReadLine();
+            Console.Write("Age= ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Gender(M/F)= ");
+            char gender = char.Parse(Console.ReadLine());
+            Console.Write("Job= ");
+            string Job = Console.ReadLine();
+            Console.Write("Emfloyment(True/False)= ");
+            bool status = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name + " Surname: " + surname + " Age: " + age + " Gender: " + gender + " Job: " + Job + " Employment: " + status);
+            */
+            /*
+            Console.Write("Exam1: ");
+            int exam1 = int.Parse(Console.ReadLine());
+            Console.Write("Exam2: ");
+            int exam2 = int.Parse(Console.ReadLine());
+            Console.Write("Exam3: ");
+            int exam3 = int.Parse(Console.ReadLine());
+
+            int average = (exam1 + exam2 + exam3) / 3;
+            bool status;
+            if (average >= 50)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+            Console.WriteLine("Average: " + average + " Passed: " + status);
+            */
+        }
+    }
+}
